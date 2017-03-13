01135d616ecfd66b33ffe7cc49dc712095ccd962
Will <wmitbrodt@gmail.com>
2017-03-12 21:01:09 -0700

    create record form now shows properly and instantaneously on page

---
+++ app/assets/javascripts/components/record_form.jsx
@@ -0,0 +1,74 @@
+RecordForm = React.createClass({
+  getInitialState() {
+    return {
+      title: '',
+      date: '',
+      amount: ''
+    };
+  },
+
+  valid() {
+    return this.state.title && this.state.date && this.state.amount;
+  },
+
+  handleChange(e) {
+    let { name } = e.target;
+    return this.setState({[name]: e.target.value});
+  },
+
+  handleSubmit(e) {
+    e.preventDefault();
+    return $.post('', { record: this.state }, data => {
+      this.props.handleNewRecord(data);
+      return this.setState(this.getInitialState());
+    }
+    , 'JSON');
+  },
+
+  render() {
+    return React.DOM.form({
+      className: 'form-inline',
+      onSubmit: this.handleSubmit
+    },
+      React.DOM.div(
+        {className: 'form-group'},
+        React.DOM.input({
+          type: 'text',
+          className: 'form-control',
+          placeholder: 'Date',
+          name: 'date',
+          value: this.state.date,
+          onChange: this.handleChange
+        })
+      ),
+      React.DOM.div(
+        {className: 'form-group'},
+        React.DOM.input({
+          type: 'text',
+          className: 'form-control',
+          placeholder: 'Title',
+          name: 'title',
+          value: this.state.title,
+          onChange: this.handleChange
+        })
+      ),
+      React.DOM.div(
+        {className: 'form-group'},
+        React.DOM.input({
+          type: 'number',
+          className: 'form-control',
+          placeholder: 'Amount',
+          name: 'amount',
+          value: this.state.amount,
+          onChange: this.handleChange
+        })
+      ),
+      React.DOM.button({
+        type: 'submit',
+        className: 'btn btn-primary',
+        disabled: !this.valid()
+      },
+        'Create record')
+    );
+  }
+});
--- app/assets/javascripts/components/records.jsx
+++ app/assets/javascripts/components/records.jsx
@@ -5,25 +5,34 @@ Records = React.createClass({
     getDefaultProps() {
       return {records: []};
     },
-   render() {
-        return React.DOM.div(
-          {className: 'records'},
-          React.DOM.h2(
-            {className: 'title'},
-            'Records'),
-          React.DOM.table(
-            {className: 'table table-bordered'},
-            React.DOM.thead(null,
-              React.DOM.tr(null,
-                React.DOM.th(null, 'Date'),
-                React.DOM.th(null, 'Title'),
-                React.DOM.th(null, 'Amount'))
-            ),
-            React.DOM.tbody(null,
-              Array.from(this.state.records).map((record) =>
-                React.createElement(Record, {key: record.id, record}))
-            )
-          )
-        );
-      }
- });
+
+    addRecord(record) {
+    let records = this.state.records.slice();
+    records.push(record);
+    return this.setState({records});
+  },
+
+  render() {
+    return React.DOM.div(
+      {className: 'records'},
+      React.DOM.h2(
+        {className: 'title'},
+        'Records'),
+      React.createElement(RecordForm, {handleNewRecord: this.addRecord}),
+      React.DOM.hr(null),
+      React.DOM.table(
+        {className: 'table table-bordered'},
+        React.DOM.thead(null,
+          React.DOM.tr(null,
+            React.DOM.th(null, 'Date'),
+            React.DOM.th(null, 'Title'),
+            React.DOM.th(null, 'Amount'))
+        ),
+        React.DOM.tbody(null,
+          Array.from(this.state.records).map((record) =>
+            React.createElement(Record, {key: record.id, record}))
+        )
+      )
+    );
+  }
+});
