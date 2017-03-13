5fc2a0f525289b78759b59fd99591c74c0524894
Will <wmitbrodt@gmail.com>
2017-03-13 01:07:14 -0700

    added destroy method on server side, added delete record method on client side

--- app/assets/javascripts/components/record.jsx
+++ app/assets/javascripts/components/record.jsx
@@ -1,8 +1,28 @@
 Record = React.createClass({
+    handleDelete(e) {
+      e.preventDefault();
+      
+      return $.ajax({
+        method: 'DELETE',
+        url: `/records/${ this.props.record.id }`,
+        dataType: 'JSON',
+        success: () => {
+          return this.props.handleDeleteRecord(this.props.record);
+        }
+      });
+    },
     render() {
-      return React.DOM.tr(null,
-        React.DOM.td(null, this.props.record.date),
-        React.DOM.td(null, this.props.record.title),
-        React.DOM.td(null, amountFormat(this.props.record.amount)));
-    }
+    return React.DOM.tr(null,
+      React.DOM.td(null, this.props.record.date),
+      React.DOM.td(null, this.props.record.title),
+      React.DOM.td(null, amountFormat(this.props.record.amount)),
+      React.DOM.td(null,
+        React.DOM.a({
+          className: 'btn btn-danger',
+          onClick: this.handleDelete
+        },
+          'Delete')
+      )
+    );
+  }
 });
--- app/assets/javascripts/components/records.jsx
+++ app/assets/javascripts/components/records.jsx
@@ -6,24 +6,36 @@ Records = React.createClass({
       return {records: []};
     },
 
-    credits() {
-      let credits = this.state.records.filter(val => val.amount >= 0);
-      return credits.reduce(((prev, curr) => prev + parseFloat(curr.amount)), 0);
+    deposits() {
+      let deposits = this.state.records.filter(val => val.amount >= 0);
+      return deposits.reduce(((prev, curr) => prev + parseFloat(curr.amount)), 0);
     },
 
-    debits() {
-      let debits = this.state.records.filter(val => val.amount < 0);
-      return debits.reduce(((prev, curr) => prev + parseFloat(curr.amount)), 0);
+    withdrawals() {
+      let withdrawals = this.state.records.filter(val => val.amount < 0);
+      return withdrawals.reduce(((prev, curr) => prev + parseFloat(curr.amount)), 0);
     },
 
     balance() {
-      return this.debits() + this.credits();
+      return this.withdrawals() + this.deposits();
     },
 
     addRecord(record) {
       let records = this.state.records.slice();
       records.push(record);
+      // setState will just update the one key of the state object
       return this.setState({records});
+    },
+
+    deleteRecord(record) {
+      // copies current component's records state
+      let records = this.state.records.slice();
+      // searches through the index to find record
+      let index = records.indexOf(record);
+      // splics it (and only it) from the array
+      records.splice(index, 1);
+      // finally, updates the component's state
+      return this.replaceState({records});
     },
 
   render() {
@@ -34,8 +46,8 @@ Records = React.createClass({
         'Records'),
         React.DOM.div(
         {className: 'row'},
-        React.createElement(AmountBox, {type: 'success', amount: this.credits(), text: 'Credit'}),
-        React.createElement(AmountBox, {type: 'danger', amount: this.debits(), text: 'Debit'}),
+        React.createElement(AmountBox, {type: 'success', amount: this.deposits(), text: 'Deposits'}),
+        React.createElement(AmountBox, {type: 'danger', amount: this.withdrawals(), text: 'Withdrawals'}),
         React.createElement(AmountBox, {type: 'info', amount: this.balance(), text: 'Balance'})),
       React.createElement(RecordForm, {handleNewRecord: this.addRecord}),
       React.DOM.hr(null),
@@ -45,11 +57,12 @@ Records = React.createClass({
           React.DOM.tr(null,
             React.DOM.th(null, 'Date'),
             React.DOM.th(null, 'Title'),
-            React.DOM.th(null, 'Amount'))
+            React.DOM.th(null, 'Amount'),
+            React.DOM.th(null, 'Actions'))
         ),
         React.DOM.tbody(null,
           Array.from(this.state.records).map((record) =>
-            React.createElement(Record, {key: record.id, record}))
+            React.createElement(Record, {key: record.id, record, handleDeleteRecord: this.deleteRecord}))
         )
       )
     );
